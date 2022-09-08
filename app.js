86e7eddaa7525c46b65813a937d6f77c7c26b02a
QbicR <111985110+QbicR@users.noreply.github.com>
2022-09-08 15:27:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const screens = document.querySelectorAll('.screen')
 const timeList = document.querySelector('#time-list')
 const timeEl = document.querySelector('#time')
 const board = document.querySelector('#board')
+const newGame = document.querySelector('#new-game-btn')
 const colors = ['#962D3E', '#348899']
+
 let time = 0
 let score = 0
 
@@ -28,13 +30,24 @@ board.addEventListener('click', event => {
     }
 })
 
+board.addEventListener('click', (event) => {
+    if (event.target.classList.contains('new-game-btn')) {
+        console.log('newgame')
+        screens[1].classList.remove('up')
+        resetGame()
+    } else if (event.target.classList.contains('new-game-btn hide')){
+        console.log(0);
+    }
+})
+let interval = 0
 function startGame() {
-    setInterval(decreaseTime, 1000)
-    createRandomCircle()
+    interval = setInterval(decreaseTime, 1000)
     setTime(time)
+    board.innerHTML = ``
+    timeEl.parentNode.classList.remove('hide')
+    createRandomCircle()
+
 }
-
-
 
 function decreaseTime() {
     if (time === 0) {
@@ -46,72 +59,51 @@ function decreaseTime() {
         }
         setTime(current)
     }
-
 }
 
 function setTime(value) {
     timeEl.innerHTML = `00:${value}`
 }
 
+function resetGame() {
+    time = 0
+    score = 0
+}
+
 function finishGame() {
-    board.innerHTML = `<h1>Счет: <span class="primary">${score}</span></h1>`
+    board.innerHTML = `<h1>Счет: <span class="primary">${score}</span></h1> <button class="new-game-btn"> Новая игра </button>`
     timeEl.parentNode.classList.add('hide')
+    clearInterval(interval)
 }
 
 function createRandomCircle() {
     if (score < 10) {
-        const circle = document.createElement('div')
-        const size = getRandomNumber(45, 60)
-        const color = getRandomColor()
-        circle.style.background = color
-        circle.style.boxShadow = `${color} 0px 0px 1px, ${color} 0px 0px 1px`
-
-        const { width, height } = board.getBoundingClientRect()
-        const x = getRandomNumber(0, width - size)
-        const y = getRandomNumber(0, height - size)
-
-        circle.classList.add('circle')
-        circle.style.width = `${size}px`
-        circle.style.height = `${size}px`
-        circle.style.top = `${y}px`
-        circle.style.left = `${x}px`
-        board.append(circle)
+        addCircle(45, 60)
     } else if (score >= 10 && score <= 25) {
-        const circle = document.createElement('div')
-        const size = getRandomNumber(25, 45)
-        const color = getRandomColor()
-        circle.style.background = color
-        circle.style.boxShadow = `${color} 0px 0px 1px, ${color} 0px 0px 1px`
-
-        const { width, height } = board.getBoundingClientRect()
-        const x = getRandomNumber(0, width - size)
-        const y = getRandomNumber(0, height - size)
-
-        circle.classList.add('circle')
-        circle.style.width = `${size}px`
-        circle.style.height = `${size}px`
-        circle.style.top = `${y}px`
-        circle.style.left = `${x}px`
-        board.append(circle)
-    } else if (score > 25){
-        const circle = document.createElement('div')
-        const size = getRandomNumber(25, 10)
-        const color = getRandomColor()
-        circle.style.background = color
-        circle.style.boxShadow = `${color} 0px 0px 1px, ${color} 0px 0px 1px`
-
-        const { width, height } = board.getBoundingClientRect()
-        const x = getRandomNumber(0, width - size)
-        const y = getRandomNumber(0, height - size)
-
-        circle.classList.add('circle')
-        circle.style.width = `${size}px`
-        circle.style.height = `${size}px`
-        circle.style.top = `${y}px`
-        circle.style.left = `${x}px`
-        board.append(circle)
+        addCircle(45, 25)
+    } else if (score > 25) {
+        addCircle(25, 10)
     }
-    
+
+}
+
+function addCircle(z, h) {
+    const circle = document.createElement('div')
+    const size = getRandomNumber(z, h)
+    const color = getRandomColor()
+    circle.style.background = color
+    circle.style.boxShadow = `${color} 0px 0px 1px, ${color} 0px 0px 1px`
+
+    const { width, height } = board.getBoundingClientRect()
+    const x = getRandomNumber(0, width - size)
+    const y = getRandomNumber(0, height - size)
+
+    circle.classList.add('circle')
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.top = `${y}px`
+    circle.style.left = `${x}px`
+    board.append(circle)
 }
 
 function getRandomNumber(min, max) {
@@ -122,6 +114,3 @@ function getRandomColor() {
     const index = Math.floor(Math.random() * colors.length)
     return colors[index]
 }
-
-
-
